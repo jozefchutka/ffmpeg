c73e3ebb4e0a05a2ec4819c1a3d224198d3f6169
Jozef Chutka <jozefchutka@gmail.com>
2021-11-19 09:29:29 +0000

    working rubberband

--- wasm/build-scripts/build-librubberband.sh
+++ wasm/build-scripts/build-librubberband.sh
@@ -15,12 +15,13 @@ CONF_FLAGS=(
 )
 echo "CONF_FLAGS=${CONF_FLAGS[@]}"
 
-#LIB_PATH1=third_party/libsamplerate
-#(cd $LIB_PATH1 && \
-#  emconfigure ./autogen.sh && \
-#  CFLAGS=$CFLAGS emconfigure ./configure "${CONF_FLAGS[@]}")
-#emmake make -C $LIB_PATH1 clean
-#emmake make -C $LIB_PATH1 install
+    #not used
+    #LIB_PATH1=third_party/libsamplerate
+    #(cd $LIB_PATH1 && \
+    #  emconfigure ./autogen.sh && \
+    #  CFLAGS=$CFLAGS emconfigure ./configure "${CONF_FLAGS[@]}")
+    #emmake make -C $LIB_PATH1 clean
+    #emmake make -C $LIB_PATH1 install
 
 LIB_PATH2=third_party/libsndfile
 #(cd $LIB_PATH2 && \
@@ -29,31 +30,19 @@ LIB_PATH2=third_party/libsndfile
 #emmake make -C $LIB_PATH2 clean
 #emmake make -C $LIB_PATH2 install
 
-LIB_PATH3=third_party/fftw3
-#(mkdir $LIB_PATH3 && cd $LIB_PATH3 && \
-#  wget http://fftw.org/fftw-3.3.3.tar.gz && \
-#  tar -xvf fftw-3.3.3.tar.gz && \
-#  cd fftw-3.3.3 && \
-#  CFLAGS=$CFLAGS emconfigure ./configure "${CONF_FLAGS[@]}")
-#emmake make -C $LIB_PATH3/fftw-3.3.3
-#emmake make -C $LIB_PATH3/fftw-3.3.3 install
-
 LIB_PATH=third_party/librubberband
-#export CPATH="${BUILD_DIR}/include"
-    #2.0.0
-    #emmake make -C $LIB_PATH -f otherbuilds/Makefile.linux clean distclean default
-(cd $LIB_PATH && \
-  CFLAGS=$CFLAGS emconfigure ./configure static "${CONF_FLAGS[@]}")
+export CPATH="${BUILD_DIR}/include"
+emmake make -C $LIB_PATH -f otherbuilds/Makefile.em clean distclean default
 
+cp $LIB_PATH/lib/librubberband.a $BUILD_DIR/lib
+cp $LIB_PATH/rubberband.pc.in $EM_PKG_CONFIG_PATH/rubberband.pc
+sed -i -e 's,%PREFIX%,'"$BUILD_DIR"',g' $EM_PKG_CONFIG_PATH/rubberband.pc
+  
+mkdir -p $BUILD_DIR/include/rubberband
+cp $LIB_PATH/rubberband/RubberBandStretcher.h $BUILD_DIR/include/rubberband/RubberBandStretcher.h
+cp $LIB_PATH/rubberband/rubberband-c.h $BUILD_DIR/include/rubberband/rubberband-c.h
 
-#cp $LIB_PATH/lib/librubberband.a $BUILD_DIR/lib
-#cp $LIB_PATH/rubberband.pc.in $EM_PKG_CONFIG_PATH/rubberband.pc
-#sed -i -e 's,%PREFIX%,'"$BUILD_DIR"',g' $EM_PKG_CONFIG_PATH/rubberband.pc
-#echo "Requires: samplerate" >> $EM_PKG_CONFIG_PATH/rubberband.pc
-
-#mkdir -p $BUILD_DIR/include/rubberband
-#cp $LIB_PATH/rubberband/RubberBandStretcher.h $BUILD_DIR/include/rubberband/RubberBandStretcher.h
-#cp $LIB_PATH/rubberband/rubberband-c.h $BUILD_DIR/include/rubberband/rubberband-c.h
-
-#cp $BUILD_DIR/lib/libsamplerate.la $BUILD_DIR/lib/librubberband.la
-#sed -i 's/libsamplerate/librubberband/g' $BUILD_DIR/lib/librubberband.la
+    #not needed
+    #echo "Requires: samplerate" >> $EM_PKG_CONFIG_PATH/rubberband.pc
+    #cp $BUILD_DIR/lib/libsamplerate.la $BUILD_DIR/lib/librubberband.la
+    #sed -i 's/libsamplerate/librubberband/g' $BUILD_DIR/lib/librubberband.la
