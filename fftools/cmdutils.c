822c68dffe08fa82d83b52b442d9ef551d1721cb
Jozef Chutka <jozefchutka@gmail.com>
2022-01-14 13:46:55 +0100

    this is not really needed as module.onExit can be used

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -135,12 +135,6 @@ void exit_program(int ret)
     if (program_exit)
         program_exit(ret);
 
-    /*
-     * Print an unique message here to detect
-     * end of operation in JavaScript.
-     */
-    printf("FFMPEG_END\n");
-
     exit(ret);
 }
 
